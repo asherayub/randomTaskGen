d980a90bc48b2b5eeee19d6e2cce42309991e2e6
Asher Ayub <99917058+asherayub@users.noreply.github.com>
2022-10-19 21:22:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+//https://apis.scrimba.com/bored/api/activity
+
+const ideaEl = document.getElementById("idea");
+const generateBtn = document.getElementById("generate-btn");
+const ideaType = document.getElementById("idea-type");
+
+const generateRandomBackground = () => {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += Math.floor(Math.random() * 9);
+  }
+  return color;
+};
+
+document.body.style.backgroundColor = generateRandomBackground();
+
+const fetchRender = () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      ideaEl.innerHTML = `<span class="idea-type">IDEA</span></br>${data.activity}`;
+      ideaType.innerHTML = `<span class="idea-type">TYPE</span></br>${data.type
+        .slice(0, 1)
+        .toUpperCase()}${data.type.slice(1).toLowerCase()}`;
+      document.body.style.backgroundColor = generateRandomBackground();
+      document.querySelector(".divider").classList.add("divider-style");
+    });
+};
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === 49) fetchRender();
+});
+generateBtn.addEventListener("click", () => {
+  fetchRender();
+});
